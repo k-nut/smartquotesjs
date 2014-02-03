8d7a19305eb1186f68698a038b96409f883e2ea6
Knut Hühne <knut@k-nut.eu>
2014-02-03 10:44:03 +0100

    add language detection

--- src/smartquotes.js
+++ src/smartquotes.js
@@ -16,17 +16,40 @@
     callback();
   }, false);
 })(function() {
+  var documentLanguage = document.documentElement.lang;
   var root = document.body;
   var node = root.childNodes[0];
+  var beginningSinglequote, beginningDoublequote, endingSinglequote, endingDoublequote;
+  switch(documentLanguage){
+  case 'de':
+    beginningSinglequote = '\u201a';
+    beginningDoublequote = '\u201e';
+    endingSinglequote = '\u2018';
+    endingDoublequote = '\u201c';
+    break;
+  case 'fr':
+    beginningSinglequote = '\u2039';
+    beginningDoublequote = '\u00ab';
+    endingSinglequote = '\u203a';
+    endingDoublequote = '\u00bb';
+    break;
+  default:
+    beginningSinglequote = '\u2018';
+    beginningDoublequote = '\u201c';
+    endingSinglequote = '\u2019';
+    endingDoublequote = '\u201d';
+    }
+
+
   while(node != null) {
     if(node.nodeType == 3) {
       node.nodeValue = node.nodeValue
-       .replace(/(\W|^)"(\S)/g, '$1\u201c$2')                                       // beginning "
-       .replace(/(\u201c[^"]*)"([^"]*$|[^\u201c"]*\u201c)/g, '$1\u201d$2')          // ending "
+       .replace(/(\W|^)"(\S)/g, '$1' + beginningDoublequote + '$2')                                       // beginning "
+       .replace(new RegExp('(' + beginningDoublequote + '[^"]*)"([^"]*$|[^' + beginningDoublequote + ']*' + beginningDoublequote + ')', "g"), '$1' + endingDoublequote + '$2')          // ending "
        .replace(/([^0-9])"/g,'$1\u201d')                                            // remaining " at end of word
-       .replace(/(\W|^)'(\S)/g, '$1\u2018$2')                                       // beginning '
-       .replace(/([a-z])'([a-z])/ig, '$1\u2019$2')                                  // conjunction's possession
-       .replace(/((\u2018[^']*)|[a-z])'([^0-9]|$)/ig, '$1\u2019$3')                 // ending '
+       .replace(/(\W|^)'(\S)/g, '$1' + beginningSinglequote + '$2')                                       // beginning '
+       .replace(/([a-z])'([a-z])/ig, '$1\u2019$2')                                   // conjunction's possession
+       .replace(new RegExp('((' + beginningSinglequote + "[^']*)|[a-z])'([^0-9]|$)", "ig"), '$1' + endingSinglequote + '$3')                 // ending '
        .replace(/(\u2018)([0-9]{2}[^\u2019]*)(\u2018([^0-9]|$)|$|\u2019[a-z])/ig, '\u2019$2$3')     // abbrev. years like '93
        .replace(/(\B|^)\u2018(?=([^\u2019]*\u2019\b)*([^\u2019\u2018]*\W[\u2019\u2018]\b|[^\u2019\u2018]*$))/ig, '$1\u2019') // backwards apostrophe
        .replace(/'''/g, '\u2034')                                                   // triple prime
